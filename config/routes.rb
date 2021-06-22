cd84a19b17bebc8b728a1ebb57dc2d063aa8657a
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-06-23 00:49:03 +0600

    feat: Add routes,view,action for My Friends option

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root "welcomes#index"
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stock' => 'stocks#search'
+  get 'my_friends' => 'users#my_friends'
 end
